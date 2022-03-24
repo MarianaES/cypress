6b12d31cd3687b8a2c0e7769530999ee04fe5714
MarianaES <mariana.enriquez.s@gmail.com>
2022-03-24 16:49:13 -0500

    Add aliases tests

--- cypress/integration/03-aliases.spec.js
+++ cypress/integration/03-aliases.spec.js
@@ -3,5 +3,44 @@
 describe('Aliases', () => {
   beforeEach(() => {
     cy.visit('/jetsetter');
+
+    // alias for the filterInput and allItems
+    cy.get('[data-test="filter-items"]').as('filterInput');
+    cy.get('[data-test="items"] ').as('allItems');
+    cy.get('[data-test="items-unpacked"]').as('unpackedItems');
+    cy.get('[data-test="items-packed"]').as('packedItems');
+  });
+
+  it('should filter items', () => {
+    // type a search term in that filter
+    cy.get('@filterInput').type('iPhone');
+
+    // verify only items matching filter are shown
+    cy.get('@allItems').each(($item) => {
+      expect($item.text()).to.include('iPhone');
+    });
+
+    // Other alternative
+    // cy.get('@allItems').should('not.contain.text', 'Hoddie');
+  });
+
+  it('should move items from one list to the other', () => {
+    cy.get('@unpackedItems').find('li').first().as('itemInQuestion');
+    cy.get('@itemInQuestion').find('label').as('itemLabel');
+    cy.get('@itemLabel').invoke('text').as('itemName');
+
+    cy.get('@itemLabel').click();
+
+    cy.get('@itemName').then((text) => {
+      cy.get('@packedItems').contains(text);
+    });
+
+    // Other alternative
+    // cy.get('@itemLabel')
+    //   .invoke('text')
+    //   .then((text) => {
+    //     cy.get('@itemLabel').click();
+    //     cy.get('@packedItems').contains(text);
+    //   });
   });
 });
